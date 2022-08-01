8447c2e024e22c0727d5ea8aa2c0887a0a20d156
kira-Developer <ab@mkka.com>
2022-08-01 05:28:22 +0300

    first commit

--- 404.php
+++ 404.php
@@ -11,5 +11,5 @@
     <div class="err2">4</div>
     <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="#">home</a> and try from there.</p></div>
       </div>
-
+</body>
 </html>
